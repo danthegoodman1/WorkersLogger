ae4c04edb45ce1ac2e95054df5d351d40402fdb4
Dan Goodman <dan@danthegoodman.com>
2023-02-23 20:27:59 -0500

    more options
    readme

---
+++ README.md
@@ -0,0 +1,64 @@
+# WorkersLogger
+
+An optimized logger for Cloudflare Workers.
+
+This allows you to capture full logs in cloudflare workers (no 150 char line limit) without slowing down your workers or having to setup a HTTP worker for HTTP logpush jobs (to proxy because of their test request).
+
+## Console Logging
+
+Logging to console (optional) can be done either as JSON or as standard `console.x` parameter flattening. See the `consoleJSON` constructor option.
+
+## With Meta
+
+An object that will be included in all `log.meta` lines. You can use this function to provide a common ID to all log lines, for example give all `line.meta` some common UUID, or attach a `RayID`.
+
+Anything you provide in the individual log call's `.meta` will override anything in the `options.withMeta` object
+
+### Usage
+
+```js
+// Create a common UUID for all log lines of this run
+const logger = new WorkersLogger({
+  withMeta: {
+    traceID: crypto.uuid()
+  }
+})
+```
+
+```js
+// Add the Cloudflare `RayID` to all log lines
+const logger = new WorkersLogger({
+  withMeta: {
+    RayID: request.headers.get("cf-ray")
+  }
+})
+```
+
+```js
+// Override something in the log line
+const logger = new WorkersLogger({
+  withMeta: {
+    AThing: "a"
+  }
+})
+logger.info("this see a", {
+  SomethingEntirelyDifferent: "b"
+})
+logger.info("this will overwrite", {
+  AThing: "c"
+})
+
+// Cloudflare log view:
+// ["INFO", "this see a", "AThing", "a", "SomethingEntirelyDifferent", "b"]
+// ["INFO", "this will overwrite", "AThing", "c"]
+```
+
+## Destination Function
+
+You may optionally provide a destination function that will bulk-write to a sink. Combined with `(event|ctx).waitUntil()` you can have this run after the response is written.
+
+### Usage
+
+```js
+
+```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export type LogLevel = "TRACE" | "DEBUG" | "INFO" | "WARN" | "ERROR"
-const logLevels = ["TRACE", "DEBUG", "INFO", "WARN", "ERROR"]
+export type LogLevel = "DEBUG" | "INFO" | "WARN" | "ERROR"
+const logLevels = ["DEBUG", "INFO", "WARN", "ERROR"]
 
 export interface WorkerLoggerOptions {
   /**
@@ -7,10 +7,36 @@ export interface WorkerLoggerOptions {
    */
   level?: LogLevel,
   /**
+   * Disables logging to the console. Default `false`
+   */
+  disableConsole?: boolean
+  /**
+   * Time used to calculate the time of the log line. Omit if you don't want to collect logs at runtime. Function must create a serialized timestamp.
+   */
+  timestampFunc?(): string | number
+  /**
+   * Override for the timestamp key. Default `_time`. Only used if `timestampFunc` is provided.
+   */
+  timestampKey?: string
+  /**
+   * Will print to the console in JSON format. By default it pretty-prints like
+   * ```
+   * console.log(level, message, Array.from(Object.entries(meta)).reduce((agg, pair) => [...agg, ...pair]))
+   * ```
+   * Example result (in cloudflare log view):
+   *
+   * ["INFO", "this is a message", "key1", "val1", "key2", 42]
+   */
+  consoleJSON?: boolean
+  /**
    * Only provides the log lines >= the current log level.
    * e.g. `TRACE` logs will be suppressed for level `DEBUG` and above.
    */
   destinationFunction?(lines: LogLine[]): Promise<void>
+  /**
+   * Optional object to join with each `log.meta`. Will be overwritten by an individual log's `.meta` properties.
+   */
+  withMeta?: { [key: string]: any }
 }
 
 export interface LogMeta {
@@ -32,31 +58,64 @@ export default class WorkerLogger {
     this.opts = options
   }
 
-  writeLog(level: LogLevel, msg: string, meta?: LogMeta) {
+  writeLog(level: LogLevel, message: string, meta?: LogMeta) {
     // Check log level - yes there is probably a better way to do this shut up
     if (logLevels.indexOf(level) < logLevels.indexOf(this.opts.level || "INFO")) {
       return
     }
+    const line: LogLine = {
+      level,
+      message,
+      meta: {
+        ...this.opts.withMeta,
+        ...meta
+      }
+    }
+    if (this.opts.timestampFunc) {
+      if (!line.meta) {
+        line.meta = {}
+      }
+      line.meta[this.opts.timestampKey || "_time"] = this.opts.timestampFunc()
+    }
+
+    this.logLines.push(line)
+    if (this.opts.disableConsole) {
+      return
+    }
+    const logContent: any[] = [level, message]
+    if (meta) {
+      logContent.push(...Array.from(Object.entries(meta)).reduce((agg, pair) => [...agg, ...pair] as any))
+    }
+    switch (level) {
+      case "DEBUG":
+        console.debug(logContent)
+        break
+      case "INFO":
+        console.info(logContent)
+        break
+      case "WARN":
+        console.warn(logContent)
+        break
+      case "ERROR":
+        console.error(logContent)
+        break
+    }
   }
 
-  trace(msg: string, meta?: LogMeta) {
-    this.writeLog("TRACE", msg, meta)
+  debug(message: string, meta?: LogMeta) {
+    this.writeLog("DEBUG", message, meta)
   }
 
-  debug(msg: string, meta?: LogMeta) {
-    this.writeLog("DEBUG", msg, meta)
+  info(message: string, meta?: LogMeta) {
+    this.writeLog("INFO", message, meta)
   }
 
-  info(msg: string, meta?: LogMeta) {
-    this.writeLog("INFO", msg, meta)
+  warn(message: string, meta?: LogMeta) {
+    this.writeLog("WARN", message, meta)
   }
 
-  warn(msg: string, meta?: LogMeta) {
-    this.writeLog("WARN", msg, meta)
-  }
-
-  error(msg: string, meta?: LogMeta) {
-    this.writeLog("ERROR", msg, meta)
+  error(message: string, meta?: LogMeta) {
+    this.writeLog("ERROR", message, meta)
   }
 
   async Drain() {
@@ -64,6 +123,6 @@ export default class WorkerLogger {
       return
     }
 
-    return this.opts.destinationFunction()
+    return this.opts.destinationFunction(this.logLines)
   }
 }
